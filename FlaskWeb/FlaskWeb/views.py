913b11c3568b67a6c48a368237edd78b55842fff
Keliz <keliz@ya.ru>
2020-05-14 22:15:42 +0300

    Add reg and chat

--- FlaskWeb/FlaskWeb/views.py
+++ FlaskWeb/FlaskWeb/views.py
@@ -4,7 +4,7 @@ import os
 
 from FlaskWeb import app
 from datetime import datetime
-from flask import Flask, render_template, flash, redirect, url_for, json
+from flask import Flask, render_template, flash, redirect, url_for, json, Response, request
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, BooleanField, SubmitField
 from flask_login import current_user, login_user, logout_user, UserMixin, LoginManager
@@ -12,7 +12,7 @@ from wtforms.validators import DataRequired
 
 app.config['SECRET_KEY'] = 'fuck_flask_and_other_python_shit'
 
-posts = []
+posts = [{ 'user':'Бот', 'message':'Всем привет' }]
 
 username = "Name"
 personalArea = "Sign In"
@@ -45,6 +45,7 @@ class User(UserMixin):
 
 
     def check_password(self, password):
+        print(hashlib.md5(password.encode('utf-8')).hexdigest())
         return hashlib.md5(password.encode('utf-8')).hexdigest() == self.passw
 
     def __repr__(self):
@@ -77,6 +78,24 @@ class User(UserMixin):
             for item in data:
                 User.userList[item['login']] = User(item)
 
+    @staticmethod
+    def addUser(_context ,username, password):
+         if username not in User.userList:
+            filename = os.path.join(_context.root_path, 'data', 'userdata.json')
+            file = open(filename, 'r')
+            data = json.load(file)
+            data += [{'login': username, 'passw': hashlib.md5(password.encode('utf-8')).hexdigest() }]
+            print(data)
+            file.close()
+
+            with open(filename, 'w') as outfile:
+                json.dump(data, outfile)
+
+            for item in data:
+                if User(item) not in User.userList:
+                    User.userList[item['login']] = User(item)
+            
+
 User.load(app)
 
 @app.route('/')
@@ -87,19 +106,26 @@ def home():
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
     someUserName = current_user.login
-    user = {'username': someUserName}
-    somePosts = [
-        {
-            'author': {'username': 'Привет'},
-            'body': 'Сообщение добавляется каждый раз при заходе на index'
-        }
-    ]
+    
+    global posts
+    
+    
     return render_template('index.html',
         title=username,       
         personalText = someUserName,        
         year=datetime.now().year,
-        logoutText = "Logout"
+        logoutText = "Logout",
+        posts = posts
     )
+
+
+@app.route('/messagePost/', methods=['GET', 'POST'])
+def messagePost():
+    global posts    
+    posts+=[{'user':current_user.login, 'message':request.form['inputText']}]
+    return redirect(url_for('home'))
+
+
 @app.route('/contact')
 def contact():
     """Renders the contact page."""    
@@ -129,8 +155,7 @@ def about():
     )
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-    global personalArea
+def login():    
     if current_user.is_authenticated:
         return redirect(url_for('userProfile'))
         # return redirect("/home")
@@ -141,20 +166,16 @@ def login():
             flash('Invalid username or password', 'error')            
             return redirect(url_for('login'))
         login_user(user)
-        somePosts = [
-            {
-                'author': {'username': form.username.data},
-                'body': form.password.data
-            }
-        ]        
-        posts.extend(somePosts)
+      
         return redirect("/home")
     return render_template('login.html',  title='Sign In', form=form)
 
 @app.route('/userProfile', methods=['GET', 'POST'])
 def userProfile():    
+    
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
+
     return render_template('userProfile.html',
                            title="Hello,"+ current_user.login,
                            message = "Whats Up? Nice log bro...",
@@ -162,13 +183,38 @@ def userProfile():
                            logoutText = "Logout"
                            )
 
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route('/registration/', methods = ['POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():        
+        user = User.load_user(form.username.data)
+        if user is None and form.password.data == form.passwordLast.data :
+            User.addUser(app, form.username.data, form.password.data)               
+            return redirect(url_for('login'))      
+              
+        return redirect("/registration")
+    return render_template('registration.html',  title='Registration', form=form)
+#or not user.check_password(form.password.data)
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    passwordLast = PasswordField('Password', validators=[DataRequired()])   
+    submit = SubmitField('Registration')
